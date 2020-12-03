ddf705b2adfc5c5784787c345c159793639bff0e
hugphat <50920961+HugPhat@users.noreply.github.com>
2020-12-03 11:50:40 +0700

    add test data script

---
+++ test_data.py
@@ -0,0 +1,25 @@
+import argparse
+from datasets.test_data import * 
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='gen data folder')
+    parser.add_argument('--inpdir', type=str, required=True,
+                        help="where to read")
+    parser.add_argument('--type', type=str, required=True, default='pretrain_tnet',
+                        help="pretrain_tnet, pretrain_mnet")
+
+    args = parser.parse_args()
+    print(args)
+    return args
+
+if __name__ == "__main__":
+    args = get_args()
+
+    data_loader = TestDataset(
+        args.inpdir, args.type, 4).train_loader
+    image, trimap, alpha = next(iter(data_loader))
+    print(image.size(), trimap.size(), alpha.size())
+    print(image)
+    print(trimap)
+    print(alpha)
