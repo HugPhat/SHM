718a5280786bac9a2d9872c0aeb88c45513a7377
root <vohungphat99@gmail.com>
2020-12-04 09:22:22 +0000

    fix inference.py

--- inference.py
+++ inference.py
@@ -3,9 +3,10 @@ import warnings
 warnings.filterwarnings('ignore')
 from utils.config import *
 import cv2
-
+from easydict import EasyDict
 from agents.shm_infer import SHMInf
 import matplotlib.pyplot as plt
+import yaml 
 
 def main():
     arg_parser = argparse.ArgumentParser()
@@ -22,8 +23,10 @@ def main():
         default=None,
         help='The Path of trimap')
     args = arg_parser.parse_args()
-    config = process_config(args.config)
-    
+
+    with open(args.config, 'r') as config_file:
+      config_dict = yaml.load(config_file)
+      config = EasyDict(config_dict)
     if config.model == 'mnet' and args.trimap == None:
         raise AssertionError('trimap is require in mnet model')
     
