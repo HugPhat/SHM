334a87bdbcc8794d9909cf3c0ea0dee6dbc5d52c
hugphat <50920961+HugPhat@users.noreply.github.com>
2020-12-04 15:47:14 +0700

    correct name main/inference

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+from agents import SHMAgent
+from utils.config import *
 import argparse
 import warnings
 warnings.filterwarnings('ignore')
-from utils.config import *
-import cv2
-from agents import SHMInf
 
-import matplotlib.pyplot as plt
 
 def main():
     arg_parser = argparse.ArgumentParser()
@@ -13,33 +11,11 @@ def main():
         '--config',
         default=None,
         help='The Path of configuration file in yaml format')
-    arg_parser.add_argument(
-        '--img',
-        default=None,
-        help='The Path of image')
-    arg_parser.add_argument(
-        '--trimap',
-        default=None,
-        help='The Path of trimap')
     args = arg_parser.parse_args()
     config = process_config(args.config)
-    
-    if config.model == 'mnet' and args.trimap == None:
-        raise AssertionError('trimap is require in mnet model')
-    
-    agent = SHMInf(config)
-    img = cv2.imread(args.img)
-    trimap = None if args.trimap is None else cv2.imread(args.trimap)
-    Img, Tri = agent.predict(img, trimap)
-    if Tri and Img:
-        f, axarr = plt.subplots(1, 2)
-        axarr[0, 0].imshow(Img)
-        axarr[0, 1].imshow(Tri)
-    elif Img:
-        plt.imshow(Img)
-    else:
-        plt.imshow(Tri)
-    plt.show()
+
+    agent = SHMAgent(config)
+    agent.run()
 
 
 if __name__ == '__main__':
